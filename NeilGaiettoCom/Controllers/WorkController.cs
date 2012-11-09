e5a4c740f31733c943cce512880a1310f555a040
neilgaietto <neil.gaietto@gmail.com>
2012-11-08 22:37:56 -0500

    initial commit

---
+++ NeilGaiettoCom/Controllers/WorkController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NeilGaiettoCom.Controllers
+{
+    public class WorkController : Controller
+    {
+        //
+        // GET: /Work/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ NeilGaiettoCom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NeilGaiettoCom.MvcApplication" Language="C#" %>
