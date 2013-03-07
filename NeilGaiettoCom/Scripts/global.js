c8ba6f6e4fd0180e13f272ac443f9b8f13170e03
neilgaietto <neil.gaietto@gmail.com>
2013-03-07 18:06:34 -0500

    tweaks to the layout

--- NeilGaiettoCom/Scripts/global.js
+++ NeilGaiettoCom/Scripts/global.js
@@ -1,1 +1,24 @@
-﻿
+﻿//Site namespace
+var NeilSite = NeilSite = NeilSite || {};
+
+//global js object
+NeilSite.Global = new (function () {
+    var me = this;
+    this.init = function () {
+        me.BindEvents();
+    }
+
+    this.BindEvents = function () {
+
+        //bind form submit links
+        $('.FormSubmitButton').click(function (e) {
+            e.preventDefault();
+            $(this).closest('form').submit();
+        });
+    }
+
+});
+
+$(document).ready(function () {
+    NeilSite.Global.init();
+});
