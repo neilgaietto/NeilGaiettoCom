9291a2e4d78cb5fb753e3b819aab5f46dec9663c
neilgaietto <neil.gaietto@gmail.com>
2013-03-14 21:45:37 -0400

    more styling

--- NeilGaiettoCom/Scripts/global.js
+++ NeilGaiettoCom/Scripts/global.js
@@ -7,15 +7,20 @@ NeilSite.Global = new (function () {
     //public
     this.init = function () {
         bindEvents();
+        setEmailLinks();
     }
 
     //private
     function bindEvents() {
     }
+    function setEmailLinks() {//sets email on all email links
+        var em = $.map("aNbecidle.fGgahijektltmon@ogpmqarislt.ucvowmx", function (n, i) { return i % 2 ? n : null; }).join("");
+        $('a.emailAddress').html(em).attr('href','mailto:'+em);
+    }
 
 });
 
-//init all objects added to namespace
+//init all objects added to namespace at initial load
 $(document).ready(function () {
     $.each(NeilSite, function (key, value) {
         if (value.init) {
